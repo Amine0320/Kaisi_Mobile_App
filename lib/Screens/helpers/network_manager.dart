d49026bf52e2f92542ee9485b12b8968b0a8e4e9
Elbah Mohamed Amine <98595599+Amine0320@users.noreply.github.com>
2024-05-26 00:13:39 +0200

    Last

--- lib/Screens/helpers/network_manager.dart
+++ lib/Screens/helpers/network_manager.dart
@@ -3,8 +3,7 @@ import 'package:get/get.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/services.dart';
 
-import '../popups/loaders.dart';
-
+import '../popups/loaders.dart'; 
 /// Manages the network connectivity status and provides methods to check and handle connectivity changes.
 class NetworkManager extends GetxController {
   static NetworkManager get instance => Get.find();
@@ -14,11 +13,11 @@ class NetworkManager extends GetxController {
   final Rx<ConnectivityResult> _connectionStatus = ConnectivityResult.none.obs; 
   /// Initialize the network manager and set up a stream to continually check the connection status.
   // @override
-  // void onInit() {
+  // void onInit() { 
   //   super.onInit();
   //   _connectivitySubscription =
   //       _connectivity.onConnectivityChanged.listen(_updateConnectionStatus); 
-  // } 
+  // }  
 
   /// Update the connection status based on changes in connectivity and show a relevant popup for no internet connection.
   Future<void> _updateConnectionStatus(ConnectivityResult result) async {
